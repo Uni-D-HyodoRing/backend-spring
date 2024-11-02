de1156b32fe1a61603e9ed6be024a3ddac1f3625
dvlp-sy <sy8389b@gmail.com>
2024-11-02 22:46:54 +0900

    [add] Create Controllers

---
+++ hyodoring/src/main/java/unid/hyodoring/web/controller/HelloController.java
@@ -0,0 +1,7 @@
+package unid.hyodoring.web.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+}
---
+++ hyodoring/src/main/java/unid/hyodoring/web/controller/UserController.java
@@ -0,0 +1,9 @@
+package unid.hyodoring.web.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+
+}
