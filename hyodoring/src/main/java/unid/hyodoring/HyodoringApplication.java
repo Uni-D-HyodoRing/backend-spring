a3e9cf8fb9faa47db5efeebd4467e755cd4a74cb
dvlp-sy <sy8389b@gmail.com>
2024-11-02 19:31:54 +0900

    [init] Initialize Spring Boot

---
+++ hyodoring/src/main/java/unid/hyodoring/HyodoringApplication.java
@@ -0,0 +1,13 @@
+package unid.hyodoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyodoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HyodoringApplication.class, args);
+	}
+
+}
---
+++ hyodoring/src/test/java/unid/hyodoring/HyodoringApplicationTests.java
@@ -0,0 +1,13 @@
+package unid.hyodoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HyodoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
